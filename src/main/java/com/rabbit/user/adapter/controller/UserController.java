0989020568b2d5173e4e297ac9daf517f28a84ba
Agustina <agustinaftorres@gmail.com>
2020-11-25 11:42:05 -0300

    ms-user

--- src/main/java/com/rabbit/user/adapter/controller/UserController.java
+++ src/main/java/com/rabbit/user/adapter/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.rabbit.user.adapter.controller;
 
+import com.rabbit.user.adapter.controller.model.UserResponse;
 import com.rabbit.user.application.service.UserService;
 import com.rabbit.user.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -14,8 +19,17 @@ public class UserController {
 
     private UserService userService;
 
-    @GetMapping
-    public User getUserById(String id){
-
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id){
+        log.info("Llamando a /api/v1/user/id para obtener usuario con id: {}", id);
+        Optional<User> user = userService.findById(id);
+        return user.get();
     }
+
+    @GetMapping("/all")
+    public List<User> getUsers(){
+        log.info("Llamando a /api/v1/user/all para obtener usuarios");
+        return userService.findAll();
+    }
+    
 }
